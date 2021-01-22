68051af4eeeae98a3cf3901e99135b3c4c1a2792
Carlos Eduardo Alves Cardoso <carloseduac910@gmail.com>
2021-01-22 01:25:36 -0300

    chore: Ajuste no deploy

--- src/database/index.ts
+++ src/database/index.ts
@@ -6,10 +6,7 @@ export default async (name = 'default'): Promise<Connection> => {
   return createConnection(
     Object.assign(defaultOptions, {
       name,
-      url:
-        process.env.NODE_ENV === 'test'
-          ? process.env.MONGO_URL_TEST
-          : process.env.MONGO_URL_DEVELOPMENT
+      url: 'mongodb+srv://mongodb:root@clusterplanets.h5k8a.mongodb.net/starwars?retryWrites=true&w=majority'
     }),
   );
 };
