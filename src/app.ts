7a356bbce56ff921c21cee5e99791b2253da7d14
Carlos Eduardo Alves Cardoso <carloseduac910@gmail.com>
2021-01-22 09:03:14 -0300

    chore: Ajuste no deploy

--- src/app.ts
+++ src/app.ts
@@ -32,6 +32,7 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
   return response.status(500).json({
     status: 'error',
     message: err,
+    env: process.env.NODE_ENV
     // message: 'Internal server error',
   });
 });
