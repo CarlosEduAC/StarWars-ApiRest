ba066530fe64c8237f1c36c2fdbfc60a3698d329
Carlos Eduardo Alves Cardoso <carloseduac910@gmail.com>
2021-01-22 01:30:56 -0300

    chore: Ajuste no deploy

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,13 +3,14 @@ import { createConnection, getConnectionOptions, Connection } from 'typeorm';
 export default async (name = 'default'): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
+  const url = process.env.NODE_ENV === 'test'
+    ? process.env.MONGO_URL_TEST
+    : process.env.MONGO_URL_DEVELOPMENT;
+
   return createConnection(
     Object.assign(defaultOptions, {
       name,
-      url:
-        process.env.NODE_ENV === 'test'
-          ? process.env.MONGO_URL_TEST
-          : process.env.MONGO_URL_DEVELOPMENT
+      url: url
     }),
   );
 };
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import app from './app';
 
-app.listen(process.env.PORT || 3333, () => {
+const port = process.env.PORT || 3333
+
+app.listen(port, () => {
   console.log('🚀 Server started on port 3333!');
 });
